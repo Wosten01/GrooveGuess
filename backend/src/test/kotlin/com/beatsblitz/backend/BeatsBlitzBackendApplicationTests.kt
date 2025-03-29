5231b725d29a94566ec98e3c254e48fd01fd2076
Vladislav Kostenko <mribloko@gmail.com>
2025-03-29 17:20:07 +0400

    Added backend SpringBoot Sample

---
+++ backend/src/test/kotlin/com/beatsblitz/backend/BeatsBlitzBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.beatsblitz.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeatsBlitzBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
